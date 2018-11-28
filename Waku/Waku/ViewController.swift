a84a91abf34ed17ee703a17bbc5c60fe04dba509
mitsuaki1229 <mitsuaki1229@gmail.com>
2018-11-28 20:33:45 +0900

    Add WKWebView.

--- Waku/Waku/ViewController.swift
+++ Waku/Waku/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var wkWebView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+        wkWebView.uiDelegate = self
+        view = wkWebView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myURL = URL(string: "https://www.google.co.jp/")
+        let myRequest = URLRequest(url: myURL!)
+        wkWebView.load(myRequest)
     }
 
 
 }
 
+extension ViewController: WKUIDelegate {}
